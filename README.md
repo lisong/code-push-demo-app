808e1636a2b70797fa21dbeceb42b442b9c7b6e2
tablee <lisong2010@gmail.com>
2017-08-10 11:35:42 +0800

    fix

--- README.md
+++ README.md
@@ -24,8 +24,8 @@ $ cd app/build/outputs/apk #install app-release.apk into your phone
 
 ```shell
 $ code-push login http://api.code-push.com:8080 #登录code-push-server
-$ code-push app add CodePushReactNativeDemo-ios  #iOS版
-$ code-push app add CodePushReactNativeDemo-android #android版
+$ code-push app add CodePushReactNativeDemo-ios ios react-native  #iOS版
+$ code-push app add CodePushReactNativeDemo-android android react-native #android版
 $ cd /path/to/code-push-demo-app
 $ npm install
 $ code-push release-react CodePushReactNativeDemo-ios ios -d Production #发布到code-push-server ios
--- android/app/src/main/java/com/codepushdemoapp/MainApplication.java
+++ android/app/src/main/java/com/codepushdemoapp/MainApplication.java
@@ -30,7 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG, "http://api.code-push.com:8080/")
+            new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG, "http://api.code-push.com/")
       );
     }
   };
